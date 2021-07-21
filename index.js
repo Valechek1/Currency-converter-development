05c797d81c9f14a77fa5b72661ef4958df24b9ed
Valechek1 <79101581094@yandex.ru>
2021-07-21 17:19:19 +0300

    Fix/ logic to the project

--- index.js
+++ index.js
@@ -1,21 +1,103 @@
-const getDataApi = async () => {
-  const response = await fetch('https://api.exchangerate.host/latest');
+const buttonsLeft = document.querySelectorAll('.btn-left-money');
+const buttonsRight = document.querySelectorAll('.btn-right-money');
+
+const selectLeft = document.querySelector('.select-left-money');
+const selectRight = document.querySelector('.select-right-money');
+
+const imgReverseArrow = document.querySelector('#arrow-img');
+
+const selects = document.querySelectorAll('#select-value');
+let btnValueLeft = 'RUB';
+let btnValueRight = 'USD';
+
+const getSupportedSymbols = async () => {
+  const response = await fetch('https://api.exchangerate.host/symbols');
   const data = await response.json();
-  return data;
+  return Object.keys(data.symbols);
 };
 
-getDataApi()
-  .then((data) => {
-    const select = document.querySelectorAll('#select-value');
-    select.forEach((element) => {
-      const keyArrayOption = Object.keys(data.rates);
-      const filterKeyArrayOption = keyArrayOption.filter((el) => el !== 'RUB' && el !== 'USD' && el !== 'EUR' && el !== 'GBP');
-      for (let i = 0; i < filterKeyArrayOption.length; i += 1) {
+const getCurrencyPair = async (currencyOne, currencyTwo) => {
+  const response = await fetch(`https://api.exchangerate.host/latest?base=${currencyOne}&symbols=${currencyTwo}`);
+  const data = await response.json();
+  const rate = data.rates[currencyTwo];
+
+  const responseRewerse = await fetch(`https://api.exchangerate.host/latest?base=${currencyTwo}&symbols=${currencyOne}`);
+  const dataRewerse = await responseRewerse.json();
+  const rateReverse = dataRewerse.rates[currencyOne];
+
+  return [rate, rateReverse];
+};
+
+// getCurrencyPairRevers('RUB', 'USD');
+// getCurrencyPair('RUB', 'EUR');
+// getCurrencyPair('RUB', 'GBP');
+
+getSupportedSymbols()
+  .then((symbols) => {
+    selects.forEach((select) => {
+      const filterSymbols = symbols.filter((el) => el !== 'RUB' && el !== 'USD' && el !== 'EUR' && el !== 'GBP');
+      for (let i = 0; i < filterSymbols.length; i += 1) {
         const optionCreate = document.createElement('option');
-        element.append(optionCreate);
-        optionCreate.innerText = filterKeyArrayOption[i];
+        optionCreate.innerText = filterSymbols[i];
+        optionCreate.value = filterSymbols[i];
+
+        select.append(optionCreate);
       }
     });
   });
 
-  
+buttonsLeft.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    btn.style.backgroundColor = '#833AE0';
+    btn.style.color = '#FFFFFF';
+    btnValueLeft = btn.innerText;
+    algoritmMeaningRightInput();
+  });
+});
+
+buttonsRight.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    btn.style.backgroundColor = '#833AE0';
+    btn.style.color = '#FFFFFF';
+    btnValueRight = btn.innerText;
+    algoritmMeaningRightInput();
+  });
+});
+
+selectLeft.addEventListener('change', (event) => {
+  btnValueLeft = event.target.value;
+  algoritmMeaningRightInput();
+});
+
+selectRight.addEventListener('change', (event) => {
+  btnValueRight = event.target.value;
+  algoritmMeaningRightInput();
+});
+
+imgReverseArrow.addEventListener('click', () => {
+  const resultLeft = btnValueLeft;
+  btnValueLeft = btnValueRight;
+  btnValueRight = resultLeft;
+  algoritmMeaningRightInput();
+});
+
+function algoritmMeaningRightInput() {
+  // const leftActiveButton = document.querySelector(`#btn-left-${btnValueLeft}`);
+  // const rightActiveButton = document.querySelector(`#btn-right-${btnValueRight}`);
+
+  // leftActiveButton.style.backgroundColor = '#833AE0';
+  // leftActiveButton.style.color = '#FFFFFF';
+
+  // rightActiveButton.style.backgroundColor = '#833AE0';
+  // rightActiveButton.style.color = '#FFFFFF';
+
+  getCurrencyPair(btnValueLeft, btnValueRight)
+    .then((rates) => {
+      console.log(rates);
+      const inputLeft = document.querySelector('#input-left');
+      const inputRight = document.querySelector('#input-right');
+      inputRight.value = rates[0] * inputLeft.value;
+    });
+}
+
+algoritmMeaningRightInput();
