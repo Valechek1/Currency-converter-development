1accc17e8305acaff40b8803c8a1eac8c22af583
Valechek1 <barynin.valentichn@yandex.ru>
2021-07-23 13:18:13 +0000

    Deploying to gh-pages from @ Valechek1/Currency-converter-development@08ed32ea04d397eb564b316ed597859c85ae0f15 🚀

---
+++ README.md
@@ -0,0 +1,5 @@
+# Currency-converter-development
+Currency converter is a convenient service for calculating a cross rate that allows you to quickly carry out various currency transforms from one currency to another.
+
+## Run 
+You can launch the project by opening `index.html` in a browser. :computer:
---
+++ index.js
@@ -0,0 +1,169 @@
+const buttonsLeft = document.querySelectorAll('.btn-left-money');
+const buttonsRight = document.querySelectorAll('.btn-right-money');
+
+const selectLeft = document.querySelector('.select-left-money');
+const selectRight = document.querySelector('.select-right-money');
+
+const imgReverseArrow = document.querySelector('#arrow-img');
+
+const selects = document.querySelectorAll('#select-value');
+
+let btnValueLeft = 'RUB';
+let btnValueRight = 'USD';
+let loaderTimer = null;
+
+const ratese = [];
+const ratesReverse = [];
+
+const inputLeft = document.querySelector('#input-left');
+const inputRight = document.querySelector('#input-right');
+
+const paragrafLeft = document.querySelector('.text-about-converter-left');
+const paragrafRight = document.querySelector('.text-about-converter-right');
+
+const showLoader = () => {
+  loaderTimer = setTimeout(() => {
+    document.querySelector('.overlay').classList.remove('hidden');
+    loaderTimer = null;
+  }, 500);
+};
+
+const hideLoader = () => {
+  if (loaderTimer !== null) {
+    clearTimeout(loaderTimer);
+    loaderTimer = null;
+  }
+  document.querySelector('.overlay').classList.add('hidden');
+};
+
+const getCurrencyPair = async (currencyOne, currencyTwo) => {
+  const response = await fetch(`https://api.exchangerate.host/latest?base=${currencyOne}&symbols=${currencyTwo}`);
+  const data = await response.json();
+  const rate = data.rates[currencyTwo];
+  const responseRewerse = await fetch(`https://api.exchangerate.host/latest?base=${currencyTwo}&symbols=${currencyOne}`);
+  const dataRewerse = await responseRewerse.json();
+  const rateReverse = dataRewerse.rates[currencyOne];
+  return [rate, rateReverse];
+};
+
+function algoritmMeaningRightInput() {
+  const leftActiveButton = document.querySelector(`#btn-left-${btnValueLeft}`);
+  const rightActiveButton = document.querySelector(`#btn-right-${btnValueRight}`);
+  const deliteLeftActivClass = document.querySelector('.left-box .activ');
+
+  if (deliteLeftActivClass) {
+    deliteLeftActivClass.classList.remove('activ');
+  }
+
+  if (leftActiveButton) {
+    leftActiveButton.classList.add('activ');
+  } else {
+    selectLeft.classList.add('activ');
+  }
+
+  const deliteRightActivClass = document.querySelector('.right-box .activ');
+  if (deliteRightActivClass) {
+    deliteRightActivClass.classList.remove('activ');
+  }
+
+  if (rightActiveButton) {
+    rightActiveButton.classList.add('activ');
+  } else {
+    selectRight.classList.add('activ');
+  }
+
+  showLoader();
+  getCurrencyPair(btnValueLeft, btnValueRight)
+    .then((rates) => {
+      ratese.push(rates[0], rates[1]);
+
+      ratesReverse.push(rates[1], rates[0]);
+
+      inputRight.value = rates[0] * inputLeft.value;
+
+      paragrafLeft.innerText = `1 ${btnValueLeft} = ${rates[0]} ${btnValueRight}`;
+
+      paragrafRight.innerText = `1 ${btnValueRight} = ${rates[1]} ${btnValueLeft}`;
+
+      hideLoader();
+    });
+  // .catch((error) => {
+  //   alert(error);
+  // });
+}
+
+const getSupportedSymbols = async () => {
+  const response = await fetch('https://api.exchangerate.host/symbols');
+  const data = await response.json();
+  return Object.keys(data.symbols);
+};
+
+buttonsLeft.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    btnValueLeft = btn.innerText;
+    algoritmMeaningRightInput();
+  });
+});
+
+buttonsRight.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    btnValueRight = btn.innerText;
+    algoritmMeaningRightInput();
+  });
+});
+
+selectLeft.addEventListener('change', (event) => {
+  btnValueLeft = event.target.value;
+  algoritmMeaningRightInput();
+});
+
+selectRight.addEventListener('change', (event) => {
+  btnValueRight = event.target.value;
+  algoritmMeaningRightInput();
+});
+
+imgReverseArrow.addEventListener('click', () => {
+  const resultLeft = btnValueLeft;
+  btnValueLeft = btnValueRight;
+  btnValueRight = resultLeft;
+  algoritmMeaningRightInput();
+});
+
+const performCalculations = () => {
+  const resultInputValue = inputLeft.value;
+  inputRight.value = ratese[0] * resultInputValue;
+};
+
+const performCalculationsRight = () => {
+  const resultInputValueRight = inputRight.value;
+  inputLeft.value = ratesReverse[1] * resultInputValueRight;
+};
+
+inputLeft.addEventListener('keyup', () => {
+  performCalculations();
+});
+
+inputRight.addEventListener('keyup', () => {
+  performCalculationsRight();
+});
+
+const startApp = () => {
+  getSupportedSymbols()
+    .then((symbols) => {
+      selects.forEach((select) => {
+        const filterSymbols = symbols.filter((el) => el !== 'RUB' && el !== 'USD' && el !== 'EUR' && el !== 'GBP');
+        for (let i = 0; i < filterSymbols.length; i += 1) {
+          const optionCreate = document.createElement('option');
+          optionCreate.innerText = filterSymbols[i];
+          optionCreate.value = filterSymbols[i];
+          select.append(optionCreate);
+        }
+      });
+    });
+  // .catch((error) => {
+  //   alert(error);
+  // });
+  algoritmMeaningRightInput();
+};
+
+startApp();
